f893deae4910a1854294722f8d08bd85cd0529ec
Ryan Butler <thebutlah@gmail.com>
2021-05-02 10:48:23 -0400

    Ran black formatter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/local/bin/python3 -u
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from dotenv import load_dotenv
+
 load_dotenv()  # take environment variables from .env
 
 from flask import Flask, abort, jsonify, request
@@ -13,17 +15,15 @@ import tensorflow as tf
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
 graph = tf.get_default_graph()
 gpt2.download_gpt2(model_name="355M")
 print("GPT downloaded")
 sess = gpt2.start_tf_sess()
 print("session started")
-gpt2.load_gpt2(sess, run_name='run1')
+gpt2.load_gpt2(sess, run_name="run1")
 print("checkpoint loaded")
-
-
 
 
 def getResponse(msg, prefixHistory):
@@ -31,20 +31,23 @@ def getResponse(msg, prefixHistory):
     app.logger.debug(f"Received request with text: {msg}")
     prefixHistory += "Person: " + msg + "\n" + "Aryeh Bookbinder:"
     with graph.as_default():
-        text = gpt2.generate(sess, length=200,temperature=0.6,prefix=prefixHistory, return_as_list=True)[0]
-    text = text[len(prefixHistory):]
+        text = gpt2.generate(
+            sess, length=200, temperature=0.6, prefix=prefixHistory, return_as_list=True
+        )[0]
+    text = text[len(prefixHistory) :]
     end = text.find("Person")
 
-    response = text[:end-1]
+    response = text[: end - 1]
     app.logger.debug(f"Responding with: {response}")
     return response, prefixHistory
 
-@app.route("/generate", methods=['POST'])
+
+@app.route("/generate", methods=["POST"])
 @cross_origin()
 def get_gen():
     data = request.get_json()
 
-    if 'text' not in data or len(data['text']) == 0 or 'model' not in data:
+    if "text" not in data or len(data["text"]) == 0 or "model" not in data:
         abort(400)
     else:
         # text = data['text']
@@ -56,10 +59,11 @@ def get_gen():
         #     prompt=text,
         #     model_name_or_path=model
         # )
-        response, _ = getResponse(data['text'], data['prefixHistory'])
-        #result = response.replace("Aryeh Bookbinder", "Aryeh Bot")
-        return jsonify({'result': "Aryeh Bookbinder:" + str(response)})
+        response, _ = getResponse(data["text"], data["prefixHistory"])
+        # result = response.replace("Aryeh Bookbinder", "Aryeh Bot")
+        return jsonify({"result": "Aryeh Bookbinder:" + str(response)})
 
-@app.route('/test')
+
+@app.route("/test")
 def test_response():
-    return 'Done', 201
+    return "Done", 201
